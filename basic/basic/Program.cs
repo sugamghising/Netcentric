e2c8919329dfaea250b3f055529c8d241dee4bcc
sugam ghising <ghisingsugam04@gmail.com>
2024-11-23 11:09:53 +0545

    swapValue assignment

---
+++ basic/basic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
